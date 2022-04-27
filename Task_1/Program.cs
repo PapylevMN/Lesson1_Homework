29f42aa8b8a4172b11a4ff152afd44eddc3188dc
Mikhail <a104s@mail.ru>
2022-04-27 13:42:00 +0300

    Task 1

---
+++ Task_1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Enter number A :");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Enter number B :");
+int b = int.Parse(Console.ReadLine());
+bool max = a > b;
+Console.WriteLine(max? "Max = " + a: "Max = " + b);
+
